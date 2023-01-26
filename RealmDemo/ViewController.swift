736c4521d36fa3564fa0f1a1f29a3ff83ac17c65
Artem Galay <iostema@mail.ru>
2023-01-26 21:42:34 +0300

    Initial Commit

---
+++ RealmDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmDemo
+//
+//  Created by Артем Галай on 26.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
